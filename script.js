77e0cba60c5f78313160b6d61f7ca9832667e1bb
Thomas Perks <perksthomas@gmail.com>
2021-04-20 04:42:35 +0100

    computerPlay function

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log ("helllo!");
+let compgo = Math.floor(Math.random() * 3) + 1;
+
+function computerPlay () {
+    if (compgo == 1) {
+        return "Rock";
+    } else if (compgo == 2) {
+        return "Paper";
+    } else  
+        return "Scissors";
+}
+
+console.log (computerPlay());
