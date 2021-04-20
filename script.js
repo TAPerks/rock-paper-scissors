904c1370c78deefd03ff447f2b9735862db7ae8c
Thomas Perks <perksthomas@gmail.com>
2021-04-20 11:38:44 +0100

    basic functions done

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
-let compgo = Math.floor(Math.random() * 3) + 1;
 
+let playerSelection = prompt ("Select Your Move:", "Rock/Paper/Scissors");
+console.log (playerSelection);
+
+let computerSelection = computerPlay ();
 function computerPlay () {
-    if (compgo == 1) {
-        return "Rock";
-    } else if (compgo == 2) {
-        return "Paper";
+    if (Math.floor(Math.random() * 3) + 1 == 1) {
+        return "rock";
+    } else if (Math.floor(Math.random() * 3) + 1 == 2) {
+        return "paper";
     } else  
-        return "Scissors";
+        return "scissors";
+}
+console.log (playerSelection);
+console.log (computerSelection);
+
+function playRound (playerSelection, computerSelection) {
+    if (playerSelection === "rock" && computerSelection === "scissors" ||
+        playerSelection === "paper" && computerSelection === "rock" ||
+        playerSelection === "scissors" && computerSelection === "paper") {
+        return "You Win!";    
+        } else if (playerSelection === "rock" && computerSelection === "paper" || 
+             playerSelection === "paper" && computerSelection === "scissors" || 
+             playerSelection === "scissors" && computerSelection === "rock") {          
+         return "You Lose!";
+        } else if (playerSelection === computerSelection) {
+            return "It's a tie!"; }
+        
 }
 
-console.log (computerPlay());
+console.log (playerSelection);
+console.log (computerSelection);
+console.log (playRound (playerSelection, computerSelection))
